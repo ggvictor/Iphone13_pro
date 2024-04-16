9241170a77c3a0b48b05cc7e50b67a15ea2a8286
ggvictor <victorrodriguessilva2@gmail.com>
2024-04-16 14:24:52 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll("#image-picker li")
+const image = document.querySelector("#product-image")
+
+// eventos
+buttons.forEach((btn)=>{
+    btn.addEventListener("click",(e)=>{
+       console.log(e)
+
+       buttons.forEach((btn)=>{
+        btn.querySelector(".color").classList.remove("selected")
+       })
+
+       const button = e.target
+
+       const id = button.getAttribute("id")
+      button.querySelector(".color").classList.add("selected")
+      image.classList.add("changing")
+      image.setAttribute("src",`img/iphone_${id}.jpg`)
+      setTimeout(() => {
+        image.classList.toggle("changing")
+      }, 200);
+    })
+})
